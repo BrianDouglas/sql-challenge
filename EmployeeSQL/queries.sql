90bceb90d3ea062bf386d01c11fdf6f3be3ce07d
BrianDouglas <bddrew86@gmail.com>
2020-12-08 12:05:35 -0800

    Tables created and data loaded

---
+++ EmployeeSQL/queries.sql
@@ -0,0 +1,11 @@
+SELECT * FROM titles
+
+SELECT * FROM employees LIMIT 20
+
+SELECT * FROM departments
+
+SELECT * FROM dept_emp LIMIT 20
+
+SELECT * FROM dept_manager
+
+SELECT * FROM salaries LIMIT 20
